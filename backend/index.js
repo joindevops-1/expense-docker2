8213233c22b7f72722851563bd09fcf84cccbb18
sivakumarreddym <info@joindevops.com>
2025-03-17 17:20:17 +0530

    expense infra

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 //Health Checking
 app.get('/health',(req,res)=>{
-    res.json("This is the health check-green-1.0.1");
+    res.json("This is the health check-blue-1.0.2");
 });
 
 // ADD TRANSACTION
